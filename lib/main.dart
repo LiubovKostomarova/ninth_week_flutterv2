b043012563c8d6ea7524c10bbad3f9225cfc2941
Liubov Kostomarova <liubov.kostomarova@gmail.com>
2022-01-15 14:09:40 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,10 @@ class HomePage extends StatefulWidget {
 }
 
 class SwitchState extends InheritedWidget {
-    bool isSwitched;
+  final bool isSwitched;
 
-  //bool isSwitched;
-  SwitchState(bool isSwitched, {
-    @required this.isSwitched,
-    @required Widget child, bool isSwitched,
-  })  : assert(isSwitched != false),
-        assert(child != null),
+  const SwitchState({@required this.isSwitched, @required Widget child})
+      : assert(child != null),
         super(child: child);
 
   @override
@@ -48,8 +44,6 @@ class SwitchState extends InheritedWidget {
 
   static bool of(BuildContext context) =>
       context.dependOnInheritedWidgetOfExactType<SwitchState>()?.isSwitched;
-  /* @override
-  bool updateShouldNotify(SwitchState old) => false;*/
 }
 
 class _HomePageState extends State<HomePage> {
@@ -65,82 +59,62 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) => Scaffold(
           body: Center(
-              child: Column(children: [SwitchState(isSwitched: isSwitched, child: 
-        Switch(
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-            activeTrackColor: Colors.pink,
-            activeColor: Colors.pinkAccent,
-            inactiveTrackColor: Colors.grey,
-            inactiveThumbColor: Colors.blue),
-        Stack(alignment: Alignment.center, children: <Widget>[
-          Image.asset(isSwitched ? cards[0].picture : cards[1].picture,
-              height: 500,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.fill),
-          Positioned(
-              bottom: 10,
-              child: Column(children: <Widget>[
-                SizedBox(
-                    width: 300.0,
-                    height: 150.0,
-                    child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: Colors.blue[100],
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                        child: Center(
-                            child: Text(
-                          isSwitched
-                              ? cards[0].description
-                              : cards[1].description,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 18,
+              child: Column(children: [
+        SwitchState(
+            isSwitched: isSwitched,
+            child: Column(children: <Widget>[
+              Switch(
+                  value: isSwitched,
+                  onChanged: (value) {
+                    setState(() {
+                      isSwitched = value;
+                    });
+                  },
+                  activeTrackColor: Colors.pink,
+                  activeColor: Colors.pinkAccent,
+                  inactiveTrackColor: Colors.grey,
+                  inactiveThumbColor: Colors.blue),
+              Stack(alignment: Alignment.center, children: <Widget>[
+                Image.asset(isSwitched ? cards[0].picture : cards[1].picture,
+                    height: 500,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.fill),
+                Positioned(
+                    bottom: 10,
+                    child: Column(children: <Widget>[
+                      SizedBox(
+                          width: 300.0,
+                          height: 150.0,
+                          child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                color: Colors.blue[100],
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                              child: Center(
+                                  child: Text(
+                                isSwitched
+                                    ? cards[0].description
+                                    : cards[1].description,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                ),
+                              )))),
+                      Container(
+                          padding: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue[200],
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        )))),
-                Container(
-                    padding: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: Colors.blue[200],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Text(
-                      isSwitched ? cards[0].name : cards[1].name,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 22,
-                      ),
-                    ))
-              ]))
-        ])
-  )])));
+                          child: Text(
+                            isSwitched ? cards[0].name : cards[1].name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 22,
+                            ),
+                          ))
+                    ]))
+              ])
+            ]))
+      ])));
 }
-
-/*class CounterBar extends StatelessWidget {
-  final int counter;
-  CounterBar({@required this.counter}) : assert(counter != null);
-
-  @override
-  Widget build(BuildContext context) => Container(
-      color: Colors.blueGrey,
-      width: 200,
-      height: 200,
-      padding: EdgeInsets.all(12.0),
-      child: Center(child: CounterText(counter: counter)));
-}
-
-class CounterText extends StatelessWidget {
-  final int counter;
-  CounterText({@required this.counter}) : assert(counter != null);
-
-  @override
-  Widget build(BuildContext context) => Container(
-      color: Colors.black,
-      padding: EdgeInsets.all(12.0),
-      child: Text("Счетчик: $counter", style: TextStyle(color: Colors.white)));
-}*/
